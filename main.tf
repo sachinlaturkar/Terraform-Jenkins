9ba14879920203a9816ccd4356ddd8212d5af38f
sachinlaturkar <107913977+sachinlaturkar@users.noreply.github.com>
2024-02-02 20:08:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
-provider "aws" {
-    region = "us-east-1"  
-}
-
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.35.0"
+      region = "us-east-1"  
+    }
+  }
 resource "aws_instance" "foo" {
   ami           = "ami-05fa00d4c63e32376" # us-west-2
   instance_type = "t2.micro"
